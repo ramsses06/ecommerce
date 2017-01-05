f387e08cb767fc14cc3867ef15329381359a2eb2
ramsses06 <ramssescorona@gmail.com>
2017-01-04 18:35:41 -0600

    ultimos detalles

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   config.action_mailer.default_url_options = { host: '162.243.28.214', port: 80 }
