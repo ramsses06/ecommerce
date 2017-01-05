d4cf40422ed1a50dbc2ead0b1ef4956570598dcb
ramsses06 <ramssescorona@gmail.com>
2017-01-04 18:53:12 -0600

    config mail

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
   domain: "gmail",
-  user_name: "",#con variables de entorno
-  password: "",#con variables de entorno
+  user_name: "ramsses.testmail@gmail.com",#con variables de entorno
+  password: "lanalanita123",#con variables de entorno
   authenticate: :login,#plain
   emable_starttls_auto: true
 }
