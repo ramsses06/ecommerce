80fa10f1d50e33a7d57f7af66931ff95a01a32b8
ramsses06 <ramssescorona@gmail.com>
2017-01-06 11:07:50 -0600

    correccion mailer gmail

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -4,6 +4,6 @@ ActionMailer::Base.smtp_settings = {
   domain: "gmail",
   user_name: "ramsses.testmail@gmail.com",#con variables de entorno
   password: "lanalanita123",#con variables de entorno
-  authenticate: :login #:plain #:login
+  authentication: :login, #:plain, #:login,
   enable_starttls_auto: true
 }
